0d98e6cd313e19b535d48b00cad781cca9a32cfa
Suhael <suhael@giffgaff.co.uk>
2019-01-12 14:19:49 +0000

    spring cloud config

---
+++ service-a/src/main/java/com/suhael/cloud/servicea/config/ServiceConfiguration.java
@@ -0,0 +1,19 @@
+package com.suhael.cloud.servicea.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "some")
+public class ServiceConfiguration {
+
+  private String property;
+
+  public String getProperty() {
+    return property;
+  }
+
+  public void setProperty(String property) {
+    this.property = property;
+  }
+}
